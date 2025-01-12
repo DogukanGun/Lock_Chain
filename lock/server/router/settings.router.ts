93629e1d0950d4146e7b583c9c8dea50edca2b1f
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2025-01-13 02:26:36 +0300

    bugfix

--- lock/server/router/settings.router.ts
+++ lock/server/router/settings.router.ts
@@ -3,13 +3,13 @@ import { generateQRAndSavePK } from "../controller/qr.controller.js";
 import { createUser } from '../controller/create.subuser.controller.js';
 import { deavtivateKey } from '../controller/deactivate.key.controller.js';
 import { unlockDoor } from '../controller/unlock.controller.js';
-import { verifyMiddleware } from 'server/middleware/verify.middleware.js';
-import { sellHome } from 'server/controller/sell.controller.js';
+import { verifyMiddleware } from '../middleware/verify.middleware.js';
+import { sellHome } from '../controller/sell.controller.js';
 
 const router = Router({ mergeParams: true });
 
 router.post("/start", generateQRAndSavePK);
-router.post("/key/create",verifyMiddleware, createUser);
+router.post("/key/create", createUser);
 router.post("/key/delete",verifyMiddleware, deavtivateKey);
 router.post("/door/unlock", unlockDoor);
 router.post("/home/sell", sellHome);
